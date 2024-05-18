7123351ab1ac343ec6b9e3aaf64b479eb0ea0ca2
EthanC <210110809@stu.hit.edu.cn>
2024-05-18 22:12:26 +0800

    first commit

---
+++ model.py
@@ -0,0 +1,5 @@
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-chinese")
+
+model = AutoModelForMaskedLM.from_pretrained("bert-base-chinese")
